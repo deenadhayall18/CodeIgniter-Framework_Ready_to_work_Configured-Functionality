2f49bed80650cbdc855d6abe3f770dd84b1e9079
deenadhayall18 <deena.dhyalan1294@gmail.com>
2018-10-11 11:07:51 +0530

    constructor error cleared

--- CodeIgniter-Framework_Ready_to_work_Configured-Functionality/application/controllers/Welcome.php
+++ CodeIgniter-Framework_Ready_to_work_Configured-Functionality/application/controllers/Welcome.php
@@ -5,10 +5,10 @@ class Welcome extends CI_Controller {
 
 	public function __construct(){
 		parent::___construct();
-		$this->load->library(array("form_validation",'session','upload','pagination'));
-		$this->load->model("Admin_model");
-		$this->emp=$this->load->database('emp', TRUE); //if more than one db used
-		$this->load->helper(array('form', 'url','security')); 
+		// $this->load->library(array("form_validation",'session','upload','pagination'));
+		// $this->load->model("Admin_model");
+		// $this->emp=$this->load->database('emp', TRUE); //if more than one db used
+		// $this->load->helper(array('form', 'url','security')); 
 	}
 	public function index()
 	{
