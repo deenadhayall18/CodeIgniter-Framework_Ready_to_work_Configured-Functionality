14ffd250cff922518188e6e5aaaee29e17a6d37c
deenadhayall18 <deena.dhyalan1294@gmail.com>
2018-10-13 11:46:31 +0530

    Constructor error cleared

--- CodeIgniter-Framework_Ready_to_work_Configured-Functionality/application/controllers/Welcome.php
+++ CodeIgniter-Framework_Ready_to_work_Configured-Functionality/application/controllers/Welcome.php
@@ -4,11 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function __construct(){
-		parent::___construct();
-		// $this->load->library(array("form_validation",'session','upload','pagination'));
+		parent::__construct();
+		$this->load->library(array("form_validation",'session','upload','pagination'));
 		// $this->load->model("Admin_model");
-		// $this->emp=$this->load->database('emp', TRUE); //if more than one db used
-		// $this->load->helper(array('form', 'url','security')); 
+		$this->emp=$this->load->database('emp', TRUE); //if more than one db used
+		$this->load->helper(array('form', 'url','security')); 
 	}
 	public function index()
 	{
